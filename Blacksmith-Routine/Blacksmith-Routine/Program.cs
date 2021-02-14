46610043280eac7975866f96cb812372821c539a
RandomGuyWhoLikesToDev <72205230+RandomGuyWhoLikesToDev@users.noreply.github.com>
2021-02-14 13:30:54 +0100

    Ajout de la solution Visual Studio

---
+++ Blacksmith-Routine/Blacksmith-Routine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Blacksmith_Routine
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
